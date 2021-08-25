fa6f77487de2dfdd5acdc3abf2227659bd2e645e
Andrew Markham <andrew.markham@lcecorp.com>
2021-08-25 15:37:14 -0400

    Cleaned up code for submission :)

--- app/src/main/java/com/example/giantbombapiproject/api/GiantBombApi.kt
+++ app/src/main/java/com/example/giantbombapiproject/api/GiantBombApi.kt
@@ -11,10 +11,6 @@ interface GiantBombApi {
         const val API_KEY = BuildConfig.GIANT_BOMB_ACCESS_kEY
     }
 
-//https://www.giantbomb.com/api/search/?api_key=9d45436f87d3848d2bdcce810bacb6df57dfd134&format=json&resources=game&query=zelda
-//https://www.giantbomb.com/api/search/?api_key=9d45436f87d3848d2bdcce810bacb6df57dfd134&format=json&resources=game&query=zelda
-
-    //    @Headers("&format=json&resources=game")
     @GET("?api_key=$API_KEY&format=json&resources=game")
     suspend fun searchGames(
         @Query("query") query: String,
